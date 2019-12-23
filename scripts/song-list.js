ea55dfcdca3b62a5bfbf37ff24618e9858b84f7b
kamalaprasai <55566861+kamalaprasai@users.noreply.github.com>
2019-12-22 21:54:59 -0500

    Assignment-12-18

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -1,21 +1,20 @@
 {
-  album.songs.forEach( (song, index) => {
+  album.songs.forEach((song, index) => {
     song.element = $(`
-      <tr>
-        <td>
-          <button>
-            <span class="song-number">${index + 1}</span>
-            <span class="ion-play"></span>
-            <span class="ion-pause"></span>
-          </button>
-        </td>
+       <tr>
+         <td>
+           <button>
+             <span class="song-number">${index + 1}</span>
+             <span class="ion-play"></span>
+             <span class="ion-pause"></span>
+           </td>
         <td>${song.title}</td>
         <td>${song.duration}</td>
-      </tr>
+       </tr>
     `);
 
     song.element.on('click', event => {
-      helper.playPauseAndUpdate(song);
+      player.playPause(song);
       $('button#play-pause').attr('playState', player.playState);
     });
 
