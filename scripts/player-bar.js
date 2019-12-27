07060cc2bcc089059009ce05839bcf21a4903ef9
kamalaprasai <55566861+kamalaprasai@users.noreply.github.com>
2019-12-26 23:30:51 -0500

    Assignment-12-18

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,50 +1,43 @@
 {
-  $('button#play-pause').on('click', function () {
-    player.playPause();
+  $('button#play-pause').on('click', function() {
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
   });
 
-  $('button#next').on('click', function () {
-    if (player.playState !== 'playing') {
-      return;
-    }
+  $('button#next').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) {
-      return;
-    }
-
+    if (nextSongIndex >= album.songs.length) { return; }
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
+  });
+
+  $('button#previous').on('click', function() {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex <= -1) { return; }
+    const previousSong = album.songs[previousSongIndex];
+    helper.playPauseAndUpdate(previousSong);
   });
 
   $('#time-control input').on('input', function (event) {
     player.skipTo(event.target.value);
   });
 
-  setInterval(() => {
-    if (player.playState !== 'playing') {
-      return;
-    }
+  $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
+  });
 
+  setInterval( () => {
+    if (player.playState !== 'playing') { return; }
     const currentTime = player.getTime();
     const duration = player.getDuration();
     const percent = (currentTime / duration) * 100;
-    $('#time-control .current-time').text(currentTime);
+    $('#time-control .current-time').text( currentTime );
     $('#time-control input').val(percent);
   }, 1000);
-
-  $('button#previous').on('click', function () {
-    if (player.playState !== 'playing') {
-      return;
-    }
-    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-    const previousSongIndex = currentSongIndex - 1;
-    if (previousSongIndex < 0) {
-      return;
-    }
-
-    const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
-  });
-}
+ }
